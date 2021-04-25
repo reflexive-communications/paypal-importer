22531c23abda1d13d0bbfa9e1c14db7fd53d6e53
akosgarai <szlem01@gmail.com>
2021-04-25 17:10:20 +0200

    Contribution transformation

--- CRM/PaypalImporter/Transformer.php
+++ CRM/PaypalImporter/Transformer.php
@@ -43,4 +43,27 @@ class CRM_PaypalImporter_Transformer
 
         return $emailData;
     }
+
+    /**
+     * Transform paypal transaction data to civicrm contribution data
+     * For the civicrm the fee_amount has to be multiplied by -1.
+     *
+     * @param array $transaction paypal transaction object
+     *
+     * @return array ContributionData
+     */
+    public static function paypalTransactionToContribution(array $transaction): array
+    {
+        $contributionData = [
+            'total_amount' => $transaction['transaction_info']['transaction_amount']['value'],
+            'fee_amount' => intval($transaction['transaction_info']['fee_amount']['value'], 10)*-1,
+            'non_deductible_amount' => $transaction['transaction_info']['transaction_amount']['value'],
+            'trxn_id' => $transaction['transaction_info']['transaction_id'],
+            'receive_date' => $transaction['transaction_info']['transaction_initiation_date'],
+            'invoice_number' => $transaction['transaction_info']['invoice_id'],
+            'source' => $transaction['cart_info']['item_details'][0]['item_name'] ?: '' ,
+        ];
+
+        return $contributionData;
+    }
 }
