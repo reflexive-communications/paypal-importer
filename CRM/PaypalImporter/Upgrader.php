631531097a19adaffedabb97a3c9dcfc25f5cca7
Sandor Semsey <sandor@es-progress.hu>
2023-03-09 01:56:02 +0100

    Upgrader: use E::LONG_NAME for extension name

--- CRM/PaypalImporter/Upgrader.php
+++ CRM/PaypalImporter/Upgrader.php
@@ -1,5 +1,7 @@
 <?php
 
+use CRM_PaypalImporter_ExtensionUtil as E;
+
 /**
  * Collection of upgrade steps.
  */
@@ -24,11 +26,11 @@ class CRM_PaypalImporter_Upgrader extends CRM_Extension_Upgrader_Base
      */
     public function install()
     {
-        $config = new CRM_PaypalImporter_Config($this->extensionName);
+        $config = new CRM_PaypalImporter_Config(E::LONG_NAME);
         // Create default configs
         if (!$config->create()) {
-            CRM_PaypalImporter_Upgrader::logError($this->extensionName.ts(' could not create configs in database'));
-            throw new CRM_Core_Exception($this->extensionName.ts(' could not create configs in database'));
+            CRM_PaypalImporter_Upgrader::logError(E::LONG_NAME.ts(' could not create configs in database'));
+            throw new CRM_Core_Exception(E::LONG_NAME.ts(' could not create configs in database'));
         }
     }
 
@@ -39,11 +41,11 @@ class CRM_PaypalImporter_Upgrader extends CRM_Extension_Upgrader_Base
      */
     public function uninstall()
     {
-        $config = new CRM_PaypalImporter_Config($this->extensionName);
+        $config = new CRM_PaypalImporter_Config(E::LONG_NAME);
         // delete current configs
         if (!$config->remove()) {
-            CRM_PaypalImporter_Upgrader::logError($this->extensionName.ts(' could not remove configs from database'));
-            throw new CRM_Core_Exception($this->extensionName.ts(' could not remove configs from database'));
+            CRM_PaypalImporter_Upgrader::logError(E::LONG_NAME.ts(' could not remove configs from database'));
+            throw new CRM_Core_Exception(E::LONG_NAME.ts(' could not remove configs from database'));
         }
     }
 }
