533c25449492366c56f88f1a0375f1fb558726c5
Sandor Semsey <sandor@es-progress.hu>
2023-03-21 17:00:05 +0100

    remove boilerplate

--- api/v3/PaypalDataImport/Process.mgd.php
+++ api/v3/PaypalDataImport/Process.mgd.php
@@ -1,8 +1,5 @@
 <?php
-// This file declares a managed database record of type "Job".
-// The record will be automatically inserted, updated, or deleted from the
-// database as appropriate. For more details, see "hook_civicrm_managed" at:
-// https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_managed
+
 return [
     [
         'name' => 'Cron:PaypalDataImport.Process',
