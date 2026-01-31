0ab43e34b141ceead9f86adeea71657a78250792
Sandor Semsey <sandor@es-progress.hu>
2026-01-31 23:58:42 +0100

    HeadlessTestCase: install extensions in one step

--- tests/phpunit/Civi/PaypalImporter/HeadlessTestCase.php
+++ tests/phpunit/Civi/PaypalImporter/HeadlessTestCase.php
@@ -14,15 +14,12 @@ class HeadlessTestCase extends TestCase implements HeadlessInterface
     /**
      * Apply a forced rebuild of DB, thus
      * create a clean DB before running tests
-     *
-     * @throws \CRM_Extension_Exception_ParseException
      */
     public static function setUpBeforeClass(): void
     {
         // Resets DB
         Test::headless()
-            ->install('rc-base')
-            ->installMe(__DIR__)
+            ->install(['rc-base', 'paypal-importer'])
             ->apply(true);
     }
 
